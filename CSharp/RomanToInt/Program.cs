3f150c1937cebe41c9f140ac00a6474f601fe9c8
d4thomas <152221911+d4thomas@users.noreply.github.com>
2025-04-12 12:40:44 -0400

    add roman to int problem

---
+++ CSharp/RomanToInt/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Solution
+{
+    public int RomanToInt(string s)
+    {
+        Dictionary<char, int> romanToInt = new Dictionary<char, int>
+        {
+            { 'I', 1 },
+            { 'V', 5 },
+            { 'X', 10 },
+            { 'L', 50 },
+            { 'C', 100 },
+            { 'D', 500 },
+            { 'M', 1000 }
+        };
+
+        int total = 0;
+        int prevValue = 0;
+
+        for (int i = s.Length - 1; i >= 0; i--)
+        {
+            int currentValue = romanToInt[s[i]];
+
+            if (currentValue < prevValue)
+            {
+                total -= currentValue;
+            }
+            else
+            {
+                total += currentValue;
+            }
+
+            prevValue = currentValue;
+        }
+
+        return total;
+    }
+
+    static void Main(string[] args)
+    {
+        Solution solution = new Solution();
+        string s = "MCMXCIV";
+        int result = solution.RomanToInt(s);
+        Console.WriteLine(result);
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # LeetCode Solutions
 LeetCode solutions are presented in C# and Python.
+
+## Problems Solved
+- Two Sum
+- Palindrome Number
+- Roman to Integer
